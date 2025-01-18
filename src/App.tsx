0d9f52e46d247725b843e80a728e099aaed97b39
WillowGeon <phillip456@naver.com>
2025-01-18 23:48:51 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,5 @@
-import React from "react";
-import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SearchPage from "../Pages/SearchPage";
-import Header from "./components/Header";
-
 const App: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/search" element=<SearchPage /> />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <></>;
 };
 
 export default App;
