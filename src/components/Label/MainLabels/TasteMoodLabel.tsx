f8f0d554d211cb0fec0b348114472166b9db3579
heithecreator <joungwon0812@gmail.com>
2025-01-20 00:08:37 +0900

    Create TasteMoodLabel.tsx

---
+++ src/components/Label/MainLabels/TasteMoodLabel.tsx
@@ -0,0 +1,9 @@
+// src/components/TasteMoodLabel.tsx
+//import React from 'react';
+import MainLabel from '../MainLabel'
+
+const TasteMoodLabel = () => {
+    return <MainLabel text="TASTE MOOD" />;
+};
+
+export default TasteMoodLabel;
