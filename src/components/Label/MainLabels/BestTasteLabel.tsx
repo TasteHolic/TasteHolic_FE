94908cb88013374dd0ec0b69968998284a184ba6
heithecreator <joungwon0812@gmail.com>
2025-01-20 00:06:52 +0900

    Create BestTasteLabel.tsx

---
+++ src/components/Label/MainLabels/BestTasteLabel.tsx
@@ -0,0 +1,9 @@
+// src/components/BestTasteLabel.tsx
+//import React from 'react';
+import MainLabel from '../MainLabel';
+
+const BestTasteLabel = () => {
+    return <MainLabel text="BEST TASTE" />;
+};
+
+export default BestTasteLabel;
