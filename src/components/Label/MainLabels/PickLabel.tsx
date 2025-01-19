f1dead25995f30181b416bac968a02baf77b05b4
heithecreator <joungwon0812@gmail.com>
2025-01-20 00:08:05 +0900

    Create PickLabel.tsx

---
+++ src/components/Label/MainLabels/PickLabel.tsx
@@ -0,0 +1,9 @@
+// src/components/PickLabel.tsx
+//import React from 'react';
+import MainLabel from '../MainLabel'
+
+const PickLabel = () => {
+    return <MainLabel text="PICK" />;
+};
+
+export default PickLabel;
