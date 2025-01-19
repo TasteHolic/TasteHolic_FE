3e08481b6ad1e99f79fdbafcda5756d0ebe631ce
heithecreator <joungwon0812@gmail.com>
2025-01-20 00:07:28 +0900

    Create HolicNowLabel.tsx

---
+++ src/components/Label/MainLabels/HolicNowLabel.tsx
@@ -0,0 +1,9 @@
+// src/components/HolicNowLabel.tsx
+
+import MainLabel from '../MainLabel';
+
+const HolicNowLabel = () => {
+    return <MainLabel text="HOLIC NOW" />;
+};
+
+export default HolicNowLabel;
