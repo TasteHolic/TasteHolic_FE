abdd81aafceb03187430c92cce9a40466ccc643a
heithecreator <joungwon0812@gmail.com>
2025-01-20 00:03:01 +0900

    Create index.ts

---
+++ src/components/icons/subLabelIcons/index.ts
@@ -0,0 +1,8 @@
+// src/components/icons/index.ts
+export { default as MusicIcon } from './MusicIcon';
+export { default as HeartIcon } from './HeartIcon';
+export { default as BirthdayHatIcon } from './BirthdayHatIcon';
+export { default as GoogleAlertsIcon } from './GoogleAlertsIcon';
+export { default as PartyBalloonIcon } from './PartyBalloonIcon';
+export { default as JingleBellIcon } from './JingleBellIcon';
+export { default as CoconutCocktailIcon } from './CoconutCocktailIcon';
