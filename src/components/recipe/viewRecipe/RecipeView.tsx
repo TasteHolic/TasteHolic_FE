9a150e6a24958089d913d2882906d87cdf021571
seoyehyun <kingyehy@yonsei.ac.kr>
2025-02-12 20:41:14 +0900

    Update RecipeView.tsx

--- src/components/recipe/viewRecipe/RecipeView.tsx
+++ src/components/recipe/viewRecipe/RecipeView.tsx
@@ -1,6 +1,7 @@
 import "./RecipeView.css"
 import ItemList from "./ViewItemList";
 import ViewWriteRecipe from "./ViewWriteRecipe";
+import ColorPicker from "../ColorPicker";
 
 interface RecipeInputProps {
     drinkName: string; //술 이름
@@ -33,7 +34,7 @@ const RecipeInput: React.FC<RecipeInputProps> = ({
 }) => {
     return (
         <>
-            <div className="viewcontainer">
+            <div className="container">
                 <div className="top">
                     <p className="drink-name">{drinkName}</p>
                     <button className="read-more-button" onClick={onReadMore}>
@@ -65,21 +66,10 @@ const RecipeInput: React.FC<RecipeInputProps> = ({
                     <div className="category">
                         <div className="category-name">색상</div>
                         <div className="colors">
-                            <div className="color1">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26" fill="none">
-                                    <circle cx="13" cy="13" r="12.5" fill="#3C2005" stroke="white" />
-                                </svg>
-                            </div>
-                            <div className="color2">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26" fill="none">
-                                    <circle cx="13" cy="13" r="12.5" fill="#AE8560" stroke="white" />
-                                </svg>
-                            </div>
-                            <div className="color3">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26" fill="none">
-                                    <circle cx="13" cy="13" r="13" fill="#636363" />
-                                </svg>
-                            </div>
+                            <ColorPicker
+                            color1="#3C2005"
+                            color2="#AE8560"
+                            color3=""/>
                         </div>
                     </div>
                     <div className="category2">
