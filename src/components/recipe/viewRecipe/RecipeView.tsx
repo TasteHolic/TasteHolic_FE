6e0f831548f397d3230235fa8ed4b99989d4fa43
seoyehyun <kingyehy@yonsei.ac.kr>
2025-02-14 19:28:39 +0900

    Update RecipeView.tsx

--- src/components/recipe/viewRecipe/RecipeView.tsx
+++ src/components/recipe/viewRecipe/RecipeView.tsx
@@ -78,8 +78,8 @@ const RecipeView: React.FC<RecipeViewProps> = ({
                             color3={hexColor3}/>
                         </div>
                     </div>
-                    <div className="category2">
-                        <div className="category-name">레시피</div>
+                    <div className="category r1">
+                        <div className="category-name r2">레시피</div>
                         <ViewWriteRecipe line1={recipeLine1} line2={recipeLine2} line3={recipeLine3} />
                     </div>
                 </div>
