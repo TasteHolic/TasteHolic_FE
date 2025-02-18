674e6e4d3664c589801af1e730dd2641cc772fba
seoyehyun <kingyehy@yonsei.ac.kr>
2025-02-18 22:55:06 +0900

    Update MyRecCocktailRecipeCard.tsx

--- src/components/MyRecCocktailRecipeCard.tsx
+++ src/components/MyRecCocktailRecipeCard.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./CocktailRecipeCard.css";
+import "./MyRecCocktailRecipeCard.css";
 
 interface CocktailRecipeCardProps {
   recipeId: number;
